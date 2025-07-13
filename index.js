6940fcdc12129e2e7ac4b8ac0818b00f6fa7bc72
Anusha Potla <anusha.potla@in.gt.com>
2025-07-13 18:55:37 +0530

    Add React frontend and update backend index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import batchRoutes from "./api/routes/batch.js";
 import { useSwagger } from "./swagger.js";
+
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
 dotenv.config();
